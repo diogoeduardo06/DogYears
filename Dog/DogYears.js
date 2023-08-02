e1463fb931591be019e31a2b1bc6e21d5660f689
diogoeduardo06 <123772268+diogoeduardo06@users.noreply.github.com>
2023-08-02 13:36:27 -0300

    Add files via upload

---
+++ Dog/DogYears.js
@@ -0,0 +1,24 @@
+// A variavel abaixo armazena a idade em anos humanos
+
+let myAge = 16;
+
+// A variavel a seguir contem os dois primeiros anos da vida de um cachorro
+
+let earlyYears = 2;
+
+// Exercicio 3
+
+earlyYears = earlyYears * 10.5;
+
+// Exercicio 4 - "laterYears" representa a diferença de idade entre a idade humana e a idade do cachorro
+
+let laterYears = myAge - 2;
+
+// O valor em "laterYears" é multiplicado por 4, quantidade de "anos de cão" que cada ano humano adicional representa
+
+laterYears = laterYears * 4;
+
+let myAgeInDogYears = laterYears + earlyYears;
+
+let myName = 'Diogo'.toLowerCase();
+console.log(`Meu nome é ${myName}. Tenho ${myAge} anos em anos humanos, que são ${myAgeInDogYears} anos em anos de cão`) 
